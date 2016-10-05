ac7b69582f20aec4b5cc2e3328afd9daea5be5a6
Raghu Betina <raghu@firstdraft.com>
2016-10-05 17:39:53 +0000

    Direct association between listings and reviews

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,9 @@
 class Listing < ApplicationRecord
   # Direct associations
 
+  has_many   :reviews,
+             :dependent => :destroy
+
   has_many   :bookings,
              :dependent => :destroy
 
