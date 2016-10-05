0da2b7609772c7118ef5b5ffef5c61786ed9e722
Raghu Betina <raghu@firstdraft.com>
2016-10-05 17:41:14 +0000

    Whitelist columns in Guest for ActiveAdmin

--- app/admin/guest.rb
+++ app/admin/guest.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Guest do
 
+ permit_params :user_id, :bookmarks, :introduction
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
