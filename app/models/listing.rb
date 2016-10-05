ab0c05aa6463a4a7c82bb9597af05488ac034c0a
Raghu Betina <raghu@firstdraft.com>
2016-10-05 17:39:52 +0000

    Direct association between listings and pictures

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,9 @@
 class Listing < ApplicationRecord
   # Direct associations
 
+  has_many   :pictures,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,8 @@
 class Picture < ApplicationRecord
   # Direct associations
 
+  belongs_to :listing
+
   # Indirect associations
 
   # Validations
